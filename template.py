d0cdf418da049881ab5e6ba72c8708ff23f7d065
Gianluca De Bonis <gianluca.de.bonis@cern.ch>
2026-05-21 17:44:54 +0200

    Fix folder name

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level = logging.INFO, format = '[%(asctime)s]: %(message)s:')
 
-project_name = 'data-science'
+project_name = 'datascience'
 
 list_of_files = [
     '.github/workflows/.gitkeep',
